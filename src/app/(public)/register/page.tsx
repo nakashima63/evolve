55e560862ed0bdc804f4d83aa52599603995b3f7
soya.nakashima <so8.63.nakaso@gmail.com>
2023-12-24 22:24:14 +0900

    #1 supabase導入

---
+++ src/app/(public)/register/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { cookies } from "next/headers";
+import { createClient } from "@/libs/supabase/server";
+import { redirect } from "next/navigation";
+
+const RegisterPage = () => {
+  const signUp = async (formData: FormData) => {
+    "use server"
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    console.log(email);
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      console.error(error);
+      return redirect("/register?message=Could not authenticate user.");
+    } 
+
+    return redirect("/");
+  };
+
+  return (
+    <div>
+      <form action={signUp}>
+        <input
+          type="email"
+          name="email"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+        />
+        <button type="submit">登録</button>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterPage;
