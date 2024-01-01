32fc64e57e66c2102a73164fba76ab076c8c772f
soya.nakashima <so8.63.nakaso@gmail.com>
2024-01-02 00:00:29 +0900

    #2 ユーザーアイコンコンポーネント作成

---
+++ src/stories/atoms/UserIcon.stories.tsx
@@ -0,0 +1,13 @@
+import { StoryObj, Meta } from "@storybook/react";
+import { UserIcon } from "../../components/atoms/UserIcon";
+
+const meta: Meta<typeof UserIcon> = {
+  component: UserIcon,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
