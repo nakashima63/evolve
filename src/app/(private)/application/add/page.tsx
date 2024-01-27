cdaf79693e6a68dc46cd4cdcc3e7a10a768f8f36
soya.nakashima <so8.63.nakaso@gmail.com>
2024-01-28 01:13:48 +0900

    #9 応募情報登録画面（モック）作成

--- src/app/(private)/application/add/page.tsx
+++ src/app/(private)/application/add/page.tsx
@@ -1,8 +1,70 @@
+import { Container } from "@/components/atoms/Container";
+import { FormItem } from "@/components/molecules/FormItem";
+import { InputForm } from "@/components/atoms/InputForm";
+
 const AddPage = () => {
   return (
-    <div>
-      <h1>Add Page</h1>
-    </div>
+    <Container>
+      <div className="py-4">
+        <h1>応募情報登録</h1>
+        <div className="border border-green-600 border-solid rounded-md p-4 mt-4 grid grid-cols-12 gap-4">
+          <FormItem formId="companyName" label="企業名" size="12">
+            <InputForm id="companyName" name="companyName" />
+          </FormItem>
+          <FormItem formId="status" label="ステータス" size="4">
+            <select
+              id="status"
+              name="status"
+              className="border border-gray-300 rounded-md w-full p-2"
+            >
+              <option value="1">選考中</option>
+              <option value="2">選考通過</option>
+              <option value="3">選考落ち</option>
+              <option value="4">内定</option>
+              <option value="5">辞退</option>
+            </select>
+          </FormItem>
+          <FormItem formId="aspirationLevel" label="志望度" size="4">
+            <select
+              id="aspirationLevel"
+              name="aspirationLevel"
+              className="border border-gray-300 rounded-md w-full p-2"
+            >
+              <option value="1">低</option>
+              <option value="2">中</option>
+              <option value="3">高</option>
+            </select>
+          </FormItem>
+          <FormItem formId="applicationRoute" label="応募経路" size="4">
+            <InputForm id="applicationRoute" name="applicationRoute" />
+          </FormItem>
+          <FormItem formId="workLocation" label="勤務地" size="12">
+            <InputForm id="workLocation" name="workLocation" />
+          </FormItem>
+          <FormItem formId="estimatedIncome" label="想定年収" size="4">
+            <InputForm id="estimatedIncome" name="estimatedIncome" />
+          </FormItem>
+          <FormItem formId="companyDetail" label="企業詳細" size="12">
+            <textarea
+              id="companyDetail"
+              name="companyDetail"
+              className="border border-gray-300 rounded-md w-full p-2"
+              rows={5}
+            />
+          </FormItem>
+          <FormItem
+            formId="estimatedIncome"
+            label="連絡先(メールアドレス)"
+            size="6"
+          >
+            <InputForm id="estimatedIncome" name="estimatedIncome" />
+          </FormItem>
+          <FormItem formId="estimatedIncome" label="連絡先(電話番号)" size="6">
+            <InputForm id="estimatedIncome" name="estimatedIncome" />
+          </FormItem>
+        </div>
+      </div>
+    </Container>
   );
 };
 
--- src/components/molecules/FormItem.tsx
+++ src/components/molecules/FormItem.tsx
@@ -4,11 +4,12 @@ interface Props {
   formId: string;
   label: string;
   children: ReactNode;
+  size?: "12" | "6" | "4" | "3" | "2" | "1";
 }
 
-export const FormItem = ({ children, formId, label }: Props) => {
+export const FormItem = ({ children, formId, label, size = "12" }: Props) => {
   return (
-    <div className="text-zinc-500">
+    <div className={`text-zinc-500 col-span-${size}`}>
       <label htmlFor={formId} className="block">
         {label}
       </label>
