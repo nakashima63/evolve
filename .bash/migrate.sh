a3df299b62f029a985fe58694c9b06d45e90a028
soya.nakashima <so8.63.nakaso@gmail.com>
2024-01-28 11:18:13 +0900

    #8 prisma導入

--- .bash/migrate.sh
+++ .bash/migrate.sh
@@ -3,9 +3,24 @@ read env_name
 echo "Enter migration name (optional):"
 read migration_name
 
+# Check if the environment file exists
+if [ ! -f ".env.${env_name}" ]; then
+  echo "Error: .env.${env_name} does not exist."
+  exit 1
+fi
+
+# Run the migration
 if [ -z "$migration_name" ]
 then
   dotenv -e .env.${env_name} -- npx prisma migrate dev
 else
   dotenv -e .env.${env_name} -- npx prisma migrate dev --name $migration_name
+fi
+
+# Check if the migration was successful
+if [ $? -eq 0 ]; then
+  dotenv -e .env.${env_name} -- npx tsx src/libs/prisma/seedTriggers.ts
+else
+  echo "Error: Migration failed."
+  exit 1
 fi
