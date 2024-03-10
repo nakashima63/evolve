2a8172c84941c8d2246d1c214347fc9f0a39102c
soya.nakashima <so8.63.nakaso@gmail.com>
2024-03-10 18:15:18 +0900

    ミドルウェア外す

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export const config = {
   matcher: "/:path*",
 };
 
-export async function middleware(req: NextRequest) {
+export async function middleware() {
   const res = NextResponse.next();
 
   // 本番環境のみBasic認証を要求
-  if (process.env.NODE_ENV === "production") {
-    const basicAuth = req.headers.get("authorization");
-    if (basicAuth) {
-      const auth = basicAuth.split(" ")[1];
-      const [user, password] = atob(auth).split(":");
+  // if (process.env.NODE_ENV === "production") {
+  //   const basicAuth = req.headers.get("authorization");
+  //   if (basicAuth) {
+  //     const auth = basicAuth.split(" ")[1];
+  //     const [user, password] = atob(auth).split(":");
 
-      if (
-        user === process.env.BASIC_AUTH_USER &&
-        password === process.env.BASIC_AUTH_PASSWORD
-      ) {
-        return res;
-      }
-    }
+  //     if (
+  //       user === process.env.BASIC_AUTH_USER &&
+  //       password === process.env.BASIC_AUTH_PASSWORD
+  //     ) {
+  //       return res;
+  //     }
+  //   }
 
-    return new Response("Auth required", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Secure Area"',
-      },
-    });
-  }
+  //   return new Response("Auth required", {
+  //     status: 401,
+  //     headers: {
+  //       "WWW-Authenticate": 'Basic realm="Secure Area"',
+  //     },
+  //   });
+  // }
   // 開発環境では認証をスキップ
   return res;
 }
