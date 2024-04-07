de627532274e68850421c40546a872d48ca6a7ea
soya.nakashima <so8.63.nakaso@gmail.com>
2024-04-07 11:42:26 +0900

    #24 day.js導入

---
+++ src/libs/dayjs/util.ts
@@ -0,0 +1,25 @@
+import dayjs from "dayjs";
+
+// 指定された月のカレンダー表を生成する関数
+export const generateCalendarForMonth = (
+  targetMonth: number = dayjs().month(),
+) => {
+  const currentYear = dayjs().year(); // 現在の年
+  // 指定月の最初の日の曜日（0=日曜日, 6=土曜日）
+  const firstDayOfWeekInMonth = dayjs(
+    new Date(currentYear, targetMonth, 1),
+  ).day();
+
+  // カレンダー表示開始日の計算
+  let dayCounter = 1 - firstDayOfWeekInMonth;
+
+  // 週ごとに日付情報を含む二次元配列を生成
+  const calendarDays = new Array(5).fill(null).map(() => {
+    return new Array(7).fill(null).map(() => {
+      dayCounter++;
+      return dayjs(new Date(currentYear, targetMonth, dayCounter));
+    });
+  });
+
+  return calendarDays;
+};
