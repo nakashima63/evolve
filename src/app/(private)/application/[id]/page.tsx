2611abf5bc9709e31528ab758ffa9194449da9ed
soya.nakashima <so8.63.nakaso@gmail.com>
2024-05-06 23:33:00 +0900

    #27 wip: suspense導入

--- src/app/(private)/application/[id]/page.tsx
+++ src/app/(private)/application/[id]/page.tsx
@@ -5,6 +5,8 @@ import { ApplicationDetail } from "./ApplicationDetail";
 import { ConfirmDeleteDialog } from "./(delete)/Dialog/ConfirmDeleteDialog";
 import { TodoListSection } from "./TodoListSection";
 import { TodoIndexDtoInterface } from "@/dtos/applications/todos/TodoIndexDto";
+import Loading from "@/app/(private)/loading";
+import { Suspense } from "react";
 
 interface Params {
   params: {
@@ -33,14 +35,18 @@ const DetailPage = async ({ params }: Params) => {
       <div className="py-4">
         <h1>応募情報詳細</h1>
         <Box>
-          <ApplicationDetail id={applicationId} />
+          <Suspense fallback={<Loading />}>
+            <ApplicationDetail id={applicationId} />
+          </Suspense>
           <div className="col-span-12 flex justify-center gap-x-4">
             <ButtonLink label="編集" href={`${applicationId}/edit`} />
             <ConfirmDeleteDialog id={applicationId} />
           </div>
         </Box>
       </div>
-      <TodoListSection todos={todos} />
+      <Suspense fallback={<Loading />}>
+        <TodoListSection todos={todos} />
+      </Suspense>
     </Container>
   );
 };
