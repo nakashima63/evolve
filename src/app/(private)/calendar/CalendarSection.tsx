b396d5903eb113ceddf5a8d74a3cc86d8ea955a8
soya.nakashima <so8.63.nakaso@gmail.com>
2024-05-11 20:29:22 +0900

    #27 Todo取得処理をサーバーコンポーネントに移動

--- src/app/(private)/calendar/CalendarSection.tsx
+++ src/app/(private)/calendar/CalendarSection.tsx
@@ -3,21 +3,11 @@ import { CalendarHeaderSection } from "./CalendarHeaderSection";
 import { CalendarContextWrapper } from "./CalendarContextWrapper";
 import { buildClient } from "@/libs/supabase/server";
 import { fetchAuthUser } from "@/libs/supabase/shared/fetchAuthUser";
+import { getTodosByUserIdService } from "@/services/applications/todos/getTodosByUserIdService";
+import { todoRepository } from "@/infrastructures/repositories/applications/todos/todoRepository";
 
 const fetchAllTodos = async (id: string) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/todo?userId=${id}`,
-    {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-      cache: "no-cache",
-    },
-  );
-
-  if (res.status === 200) {
-    const data = await res.json();
-    return data.todos;
-  }
+  return await getTodosByUserIdService(id, todoRepository());
 };
 
 export const CalendarSection = async () => {
